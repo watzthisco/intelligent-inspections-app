b8ca569779af4a9fa4c52ea7dfe5cea7cdce74c8
Pluralsight <pluralsight@Mac-mini.local>
2016-08-31 11:23:56 -0700

    working on indexdb example

---
+++ Module 4 - End/js/IDBExample.js
@@ -0,0 +1,48 @@
+if (!window.indexedDB) {
+    window.alert("Your browser doesn't support IndexedDB");
+}
+
+var db;
+var request = window.indexedDB.open("MyFirstDatabase", 1);
+
+request.onupgradeneeded = function (event) {
+    console.log("running onupgradeneeded");
+    var myDB = event.target.result;
+
+
+    if (!myDB.objectStoreNames.contains("inspections")) {
+        myDB.createObjectStore("inspections");
+    }
+};
+
+request.onerror = function (event) {
+    // do something with request.ErrorCode
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+    document.querySelector("#submitButton").addEventListener("click", addInspection);
+};
+
+function addInspection(event) {
+
+    var transaction = db.transaction(["inspections"], "readwrite");
+    var store = transaction.objectStore("inspections");
+
+    var inspection = {
+        prop_id: "4",
+        inspect_date: "November 3, 2016",
+        int_walls: 5,
+        int_walls_notes: "Has walls"
+    };
+
+    var request = store.add(inspection, 1);
+
+    request.onerror = function (event) {
+        console.log("Error", event.target.error.name);
+    };
+
+    request.onsuccess = function (event) {
+        console.log("Request completed!");
+    };
+}
--- sw.js
+++ sw.js
@@ -33,9 +33,15 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
     event.respondWith(
+        // caches is a read-only property of the worker that returns the
+        // CacheStorage object associated with the current worker context
+
+        //the match method checks if a given request is a key in any of the cache objects
+        //if so, it returns a promise that resolves to the matching response.
+
         caches.match(event.request)
             .then(function(response) {
-                // Cache hit - return response
+                // If the response to this request is in the cache, return it.
                 if (response) {
                     return response;
                 }
@@ -46,9 +52,11 @@ self.addEventListener('fetch', function(event) {
                 // to clone the response
                 var fetchRequest = event.request.clone();
 
+                //response wasn't cached, so use fetch to get perform the request and get a response.
+
                 return fetch(fetchRequest).then(
                     function(response) {
-                        // Check if we received a valid response
+                        // Check if we received a valid response. If not return what the response was.
                         if(!response || response.status !== 200 || response.type !== 'basic') {
                             return response;
                         }
@@ -59,11 +67,14 @@ self.addEventListener('fetch', function(event) {
                         // to clone it so we have 2 stream.
                         var responseToCache = response.clone();
 
+                        // We got a response from the request and we've cloned it.
+                        // Open the cache, put the cloned response in the cache
+
                         caches.open(CACHE_NAME)
                             .then(function(cache) {
                                 cache.put(event.request, responseToCache);
                             });
-
+                        // then send the page to the browser.
                         return response;
                     }
                 );
