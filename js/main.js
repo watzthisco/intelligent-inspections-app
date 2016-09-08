99ec4c24e70b7cb5940276666cb5220959b59e58
Pluralsight <pluralsight@Mac-mini.local>
2016-09-08 10:32:05 -0700

    wrking on module 4

--- js/main.js
+++ js/main.js
@@ -10,4 +10,57 @@ $(document).ready(function() {
 
     })
 
+
+// save data to local database
+
+    var db = new Dexie('intelligentInspections');
+    console.log("db created");
+    db.version(1).stores({
+        inspections: '++id'
+    });
+    var formObj = {};
+    db.inspections.put(formObj);
+
+    db.open().then(function () {
+        // populate form - need to figure out how to load radio buttons, or just scrap this
+        db.inspections.toArray().then(function (fData) {
+            fData = fData[fData.length-1];
+            $.each(fData, function (formEle, formEleVal) {
+                $('[name=' + formEle + ']').val(formEleVal);
+            });
+        });
+    });
+
+// on blur of any form field, save the form data to indexedDB
+    $('#newInspection input, #newInspection textarea').on('blur', function () {
+        console.log('input blurred');
+// this can be cleaned up better to save
+// only the relevant form data
+// I am saving the entire form data for simplicity
+// convert Form data to Object
+// http://stackoverflow.com/a/17784656/1015046
+        formObj = $('form').serializeObject();
+        //todo: figure out how to save the images
+        //https://hacks.mozilla.org/2012/02/storing-images-and-files-in-indexeddb/
+
+        db.transaction("rw", db.inspections, function() {
+            //todo: find a better way to find the last one. maybe look for the property id?
+            db.inspections.where("id").aboveOrEqual(1).last(function(last){
+                db.inspections.update(last,formObj);
+            });
+        })
+
+    });
+
+    var options = {
+        target: '#output',
+        success: function () {
+            db.delete();
+            console.log("success saving to server and local db deleted");
+        },
+        failure: function () {
+            console.log("could not save to server. Will try again when you're online");
+        }
+    };
+    $('#newInspection').ajaxForm(options);
 });
--- saveInspection.php
+++ saveInspection.php
@@ -1,51 +1,3 @@
-<!DOCTYPE html>
-<html class="no-js">
-    <head>
-        <meta charset="utf-8">
-        <title>Inspection Saved</title>
-        <meta name="description" content="">
-        <meta name="HandheldFriendly" content="sectionue">
-        <meta name="MobileOptimized" content="320">
-        <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
-        <meta http-equiv="cleartype" content="on">
-
-        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/touch/apple-touch-icon-144x144-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/touch/apple-touch-icon-114x114-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/touch/apple-touch-icon-72x72-precomposed.png">
-        <link rel="apple-touch-icon-precomposed" href="img/touch/apple-touch-icon-57x57-precomposed.png">
-        <link rel="shortcut icon" sizes="196x196" href="img/touch/touch-icon-196x196.png">
-        <link rel="shortcut icon" href="img/touch/apple-touch-icon.png">
-
-        <!-- Tile icon for Win8 (144x144 + tile color) -->
-        <meta name="msapplication-TileImage" content="img/touch/apple-touch-icon-144x144-precomposed.png">
-        <meta name="msapplication-TileColor" content="#222222">
-
-        <!-- SEO: If mobile URL is different from desktop URL, add a canonical link to the desktop page -->
-        <!--
-        <link rel="canonical" href="http://www.example.com/" >
-        -->
-
-        <!-- Add to homescreen for Chrome on Android -->
-
-        <meta name="mobile-web-app-capable" content="yes">
-
-
-        <!-- For iOS web apps. Delete if not needed. https://github.com/h5bp/mobile-boilerplate/issues/94 -->
-        <meta name="apple-mobile-web-app-capable" content="yes">
-        <meta name="apple-mobile-web-app-status-bar-style" content="black">
-        <meta name="apple-mobile-web-app-title" content="">
-
-        <!-- This script prevents links from opening in Mobile Safari. https://gist.github.com/1042026 -->
-
-        <script>(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.prevendivefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")</script>
-
-
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="css/main.css">
-        <script src="js/vendor/modernizr-2.7.1.min.js"></script>
-    </head>
-    <body>
-    <h2>Inspection Saved</h2>
 <?php
 /*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
@@ -57,7 +9,7 @@ $target = "pics/";
 
 
 // Connects to your Database
-mysql_connect("mysql.codingjsfordummies.com", "intelinspector", "inspectme!") or die(mysql_error()) ;
+mysql_connect("localhost", "inspector", "password") or die(mysql_error()) ;
 mysql_select_db("intelligentinspections") or die(mysql_error()) ;
 
 
@@ -67,47 +19,46 @@ mysql_select_db("intelligentinspections") or die(mysql_error()) ;
 	$inspectionValues = array();
 
 foreach($_POST as $key => $value)
-	{
-		if($key != "saveForm") //Prevent the submit button's name and value from being inserted into the db
-		{
-			//echo "Key: ".$key."<br>";
-			
-				$value=mysql_real_escape_string($_POST[$key]);
-				array_push($inspectionFields,$key);
-				array_push($inspectionValues,$value);
-		}
+{
+if(($key != "saveForm") && (substr($key, -7) != "picture"))//Prevent the submit button's name and value from being inserted into the db
+{
+//echo "Key: ".$key."<br>";
 
-			
-	}
-	
-	$fieldsString = implode(',',$inspectionFields);
-	$valuesString = "\"".implode('","',$inspectionValues)."\"";
+$value=mysql_real_escape_string($_POST[$key]);
+array_push($inspectionFields,$key);
+array_push($inspectionValues,$value);
+}
 
 
-	$query = "INSERT INTO inspections ($fieldsString) VALUES ($valuesString)";
-	//echo $query;
-	mysql_query($query) or die(mysql_error());
+}
 
-	//insert and move pictures
-	foreach($_FILES as $file){
-    	$file_name = microtime(true).$file['name'];
-    	$file_url = $target.basename($file_name);
-    	
+$fieldsString = implode(',',$inspectionFields);
+$valuesString = "\"".implode('","',$inspectionValues)."\"";
 
 
-		if( !empty( $file['tmp_name'] ) && is_uploaded_file( $file['tmp_name'] ) )
-        {
-        	$query = "INSERT INTO pictures (prop_id,file_url) VALUES ('$prop_id', '$file_url')";
+$query = "INSERT INTO inspections ($fieldsString) VALUES ($valuesString)";
+//echo $query;
+mysql_query($query) or die(mysql_error());
 
-			mysql_query($query);
-            // the path to the actual uploaded file is in $_FILES[ 'image' ][ 'tmp_name' ][ $index ]
-            // do something with it:
-			echo "Uploading ".$file_name."<br>";
-            move_uploaded_file( $file['tmp_name'], $file_url ); // move to new location perhaps?
-        }
+//insert and move pictures
+foreach($_FILES as $file){
 
-    }
+$file_url = $target.basename( $file['name']);
+
+
+
+if( !empty( $file['tmp_name'] ) && is_uploaded_file( $file['tmp_name'] ) )
+{
+$query = "INSERT INTO pictures (prop_id,file_url) VALUES ('$prop_id', '$file_url')";
+
+mysql_query($query);
+// the path to the actual uploaded file is in $_FILES[ 'image' ][ 'tmp_name' ][ $index ]
+// do something with it:
+echo "Uploading ".$file['name']."<br>";
+move_uploaded_file( $file['tmp_name'], $file_url ); // move to new location perhaps?
+}
+
+}
 
 ?>
-</body>
-</html>
+<h1>Success!</h1>
