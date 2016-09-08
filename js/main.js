0af3b4ed356e492cba5c3c5f2747c78201c373d2
Pluralsight <pluralsight@Mac-mini.local>
2016-09-08 12:14:37 -0700

    more fixes

--- js/main.js
+++ js/main.js
@@ -8,22 +8,29 @@ $(document).ready(function() {
 
         $(event.target).parent().children("div").toggleClass("active-section");
 
-    })
+    });
 
+    var formObj = {};
 
 // save data to local database
-
     var db = new Dexie('intelligentInspections');
     console.log("db created");
     db.version(1).stores({
-        inspections: '++id'
+        inspections: '++id, prop_id'
     });
-    var formObj = {};
-    db.inspections.put(formObj);
+
 
     db.open().then(function () {
-        // populate form - need to figure out how to load radio buttons, or just scrap this
         db.inspections.toArray().then(function (fData) {
+
+
+
+            if (fData.length===0) {
+                db.inspections.put(formObj);
+                console.log("its 0");
+            }
+
+            // populate form - need to figure out how to load radio buttons, or just scrap this
             fData = fData[fData.length-1];
             $.each(fData, function (formEle, formEleVal) {
                 $('[name=' + formEle + ']').val(formEleVal);
@@ -39,17 +46,21 @@ $(document).ready(function() {
 // I am saving the entire form data for simplicity
 // convert Form data to Object
 // http://stackoverflow.com/a/17784656/1015046
-        formObj = $('form').serializeObject();
         //todo: figure out how to save the images
         //https://hacks.mozilla.org/2012/02/storing-images-and-files-in-indexeddb/
 
-        db.transaction("rw", db.inspections, function() {
-            //todo: find a better way to find the last one. maybe look for the property id?
-            db.inspections.where("id").aboveOrEqual(1).last(function(last){
-                db.inspections.update(last,formObj);
-            });
-        })
+        db.transaction("rw", db.inspections, function () {
 
+            db.inspections.toArray().then(function (fData) {
+                console.log("fdata length: " + fData.length);
+
+
+                fData = fData[fData.length - 1];
+                formObj = $('form').serializeObject();
+
+                db.inspections.update(fData.id, formObj);
+            });
+        });
     });
 
     var options = {
@@ -63,4 +74,5 @@ $(document).ready(function() {
         }
     };
     $('#newInspection').ajaxForm(options);
+
 });
