971f2e641aeddc7c5bc7506f430134eab6b49ded
Pluralsight <pluralsight@Mac-mini.local>
2016-09-12 13:22:45 -0700

    finishing module 4

--- Module 4 - End/js/main.js
+++ Module 4 - End/js/main.js
@@ -8,6 +8,45 @@ $(document).ready(function () {
         $(event.target).siblings().toggleClass("active-section");
     });
 
+// save data to local database
+
+    var formObj = {};
+
+    var db = new Dexie('intelligentInspections');
+    db.version(1).stores({
+        inspections: '++id'
+    });
+
+    db.open().then(function () {
+        db.inspections.toArray().then(function (fData) {
+            if (fData.length === 0) {
+                db.inspections.put(formObj);
+                console.log ("inserted blank record");
+            }
+        })
+    });
+
+
+    $('#newInspection input, #newInspection textarea').on('blur', function(){
+        console.log('input blurred');
+
+        db.transaction("rw", db.inspections, function() {
+            db.inspections.toArray().then(function (fData) {
+
+                fData = fData[fData.length - 1];
+
+                formObj = $('form').serializeObject();
+
+                db.inspections.update(fData.id, formObj);
+
+            });
+        });
+    });
+
+
+
+
+
 
 
     //bind the form
--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    var sectionHeads = $("section div:first-child");
+    //show the section headers
+    sectionHeads.show();
 
-
-    $("section div:first-child").show();
-
-
-    $("section div:first-child").click(function(event){
-
-        $(event.target).parent().children("div").toggleClass("active-section");
-
+    //expand and collapse form sections
+    sectionHeads.click(function (event) {
+        $(event.target).siblings().toggleClass("active-section");
     });
 
 // save data to local database
@@ -15,45 +13,28 @@ $(document).ready(function() {
     var formObj = {};
 
     var db = new Dexie('intelligentInspections');
-    console.log("db created");
     db.version(1).stores({
         inspections: '++id'
     });
 
     db.open().then(function () {
         db.inspections.toArray().then(function (fData) {
-
-            if (fData.length===0) {
+            if (fData.length === 0) {
                 db.inspections.put(formObj);
-                console.log("its 0");
+                console.log ("inserted blank record");
             }
-
-            // populate form - need to figure out how to load radio buttons, or just scrap this
-            fData = fData[fData.length-1];
-            $.each(fData, function (formEle, formEleVal) {
-                $('[name=' + formEle + ']').val(formEleVal);
-            });
-        });
+        })
     });
 
-// on blur of any form field, save the form data to indexedDB
+
     $('#newInspection input, #newInspection textarea').on('blur', function () {
         console.log('input blurred');
-// this can be cleaned up better to save
-// only the relevant form data
-// I am saving the entire form data for simplicity
-// convert Form data to Object
-// http://stackoverflow.com/a/17784656/1015046
-        //todo: figure out how to save the images
-        //https://hacks.mozilla.org/2012/02/storing-images-and-files-in-indexeddb/
 
         db.transaction("rw", db.inspections, function () {
-
             db.inspections.toArray().then(function (fData) {
-                console.log("fdata length: " + fData.length);
-
 
                 fData = fData[fData.length - 1];
+
                 formObj = $('form').serializeObject();
 
                 db.inspections.update(fData.id, formObj);
@@ -61,14 +42,17 @@ $(document).ready(function() {
         });
     });
 
+
+
+
+
+
+
+    //bind the form
     var options = {
         target: '#output',
         success: function () {
-            db.delete();
-            console.log("success saving to server and local db deleted");
-        },
-        failure: function () {
-            console.log("could not save to server. Will try again when you're online");
+            //db.delete();
         }
     };
     $('#newInspection').ajaxForm(options);
