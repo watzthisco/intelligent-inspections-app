c516a3b8fee618be6de40a84b44dcc2c5e4d8250
chrisminnick <chris@watzthis.com>
2017-04-26 08:57:48 -0700

    cleaning up

--- intelligent-inspections/includes/dbconn.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Connects to your Database
-$con = mysqli_connect("localhost", "inspector", "password") or die(mysqli_error($con)) ;
-mysqli_select_db($con, "intelligentinspections") or die(mysqli_error($con)) ;
-?>
